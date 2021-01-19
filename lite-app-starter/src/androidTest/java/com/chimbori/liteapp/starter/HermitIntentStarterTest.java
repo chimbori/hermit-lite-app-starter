882adeda92798c1ba08d24c6340b583132a7d79c
Chimbori <hello@chimbori.com>
2021-01-18 16:09:01 -0800

    Remove support for passing a Lite App as an Asset URI. In the interest of simplicity, users can just install their own Lite App first, and then use it from the Intent.

--- lite-app-starter/src/androidTest/java/com/chimbori/liteapp/starter/HermitIntentStarterTest.java
+++ lite-app-starter/src/androidTest/java/com/chimbori/liteapp/starter/HermitIntentStarterTest.java
@@ -4,13 +4,13 @@ import android.content.Intent;
 
 import org.junit.Test;
 
-import static com.chimbori.liteapp.starter.HermitIntentStarter.createSimpleLiteAppIntent;
+import static com.chimbori.liteapp.starter.HermitIntentStarter.createLiteAppIntent;
 import static org.junit.Assert.assertEquals;
 
 public class HermitIntentStarterTest {
   @Test
   public void testCreateSimpleLiteAppIntent() {
-    Intent actualIntent = createSimpleLiteAppIntent("https://example.org/");
+    Intent actualIntent = createLiteAppIntent("https://example.org/");
     assertEquals(Intent.ACTION_VIEW, actualIntent.getAction());
     assertEquals("https://example.org/", actualIntent.getDataString());
     assertEquals("com.chimbori.hermitcrab/com.chimbori.hermitcrab.WebActivity", actualIntent.getComponent().flattenToString());
